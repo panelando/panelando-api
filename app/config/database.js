2e5232ea2654e1e440f5d2fecb234dd795363d1f
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-08-16 22:44:46 -0300

    Add postgres config

---
+++ app/config/database.js
@@ -0,0 +1,8 @@
+export default {
+  host: 'ec2-54-197-250-52.compute-1.amazonaws.com',
+  database: 'df8q2mq9fec7h0',
+  user: 'nsikicettshvsk',
+  port: 5432,
+  password: '2lGzVqapAuR6tib23sldFHQ7bv'
+}
+
