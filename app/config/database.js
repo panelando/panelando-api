d60236f99983c788b56c2c2bdbfd8a6c24a03136
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-09-07 14:35:54 -0300

    Fix config database

--- app/config/database.js
+++ app/config/database.js
@@ -1,8 +1,9 @@
 export default {
-  host: 'ec2-54-197-250-52.compute-1.amazonaws.com',
-  database: 'df8q2mq9fec7h0',
-  user: 'nsikicettshvsk',
+  host: 'db',
   port: 5432,
-  password: '2lGzVqapAuR6tib23sldFHQ7bv'
+  dialect: 'postgres',
+  database: 'postgres',
+  username: 'postgres',
+  password: 'panelando'
 }
 
