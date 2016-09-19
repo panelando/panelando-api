e4664ba543feedb13b68c278cc93a5e65755089c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-09-18 22:12:36 -0300

    Fix modules and res.sendStatus

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize'
-import config from 'config/database'
+import config from '../config/database'
 
 const database = new Sequelize(config)
 
--- app/modules/menus/controller.js
+++ app/modules/menus/controller.js
@@ -3,34 +3,31 @@ import menu from './model'
 export function index (req, res) {
   menu.findAll()
     .then(ms => res.send(ms))
-    .catch(err => res.status(500))
+    .catch(err => res.sendStatus(500))
 }
 
 export function show (req, res) {
   menu.findById(req.params.id)
     .then(m => res.send(m))
-    .catch(err => res.status(500))
+    .catch(err => res.sendStatus(500))
 }
 
 export function create (req, res) {
   menu.create(req.body)
     .then(m => res.send(m))
-    .catch(err => res.status(500))
+    .catch(err => res.sendStatus(500))
 }
 
 export function update (req, res) {
   menu.findById(req.params.id)
     .then(m => m.updateAttributes(req.body))
     .then(m => res.send(m))
-    .catch(err => res.status(500))
+    .catch(err => res.sendStatus(500))
 }
 
 export function remove (req, res) {
-  const where = {
-    id: req.param.id
-  }
-
-  menu.delete(req.param.id, { where })
-    .then(m => res.send(m))
-    .catch(err => res.status(500))
+  menu.findById(req.params.id)
+    .then(m => m.destroy(req.body))
+    .then(m => res.sendStatus(200))
+    .catch(err => res.sendStatus(500))
 }
