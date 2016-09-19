f52018479dcfab1ab31e106dd5006341b960d37a
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-09-19 18:52:19 -0300

    Improve error handling on controllers

--- app/modules/recipes/controller.js
+++ app/modules/recipes/controller.js
@@ -3,31 +3,31 @@ import recipe from './model'
 export function index (req, res) {
   recipe.findAll()
     .then(xs => res.send(xs))
-    .catch(err => res.sendStatus(500))
+    .catch(err => res.status(500).send(err))
 }
 
 export function show (req, res) {
   recipe.findById(req.params.id)
     .then(x => res.send(x))
-    .catch(err => res.sendStatus(500))
+    .catch(err => res.status(500).send(err))
 }
 
 export function create (req, res) {
   recipe.create(req.body)
     .then(x => res.send(x))
-    .catch(err => res.sendStatus(500))
+    .catch(err => res.status(500).send(err))
 }
 
 export function update (req, res) {
   recipe.findById(req.params.id)
     .then(x => x.updateAttributes(req.body))
     .then(x => res.send(x))
-    .catch(err => res.sendStatus(500))
+    .catch(err => res.status(500).send(err))
 }
 
 export function remove (req, res) {
   recipe.findById(req.params.id)
     .then(x => x.destroy(req.body))
-    .then(() => res.sendStatus(200))
-    .catch(err => res.sendStatus(500))
+    .then(() => res.status(200))
+    .catch(err => res.status(500).send(err))
 }
