b6197485f253162a7c25de7578248cb247d2f769
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-10-11 10:33:59 -0300

    Add new resources to app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
-import menus from './routes/menu'
+
+import category from './routes/category'
+import comment from './routes/comment'
+import ingredient from './routes/ingredient'
+import instruction from './routes/instruction'
+import menu from './routes/menu'
+import recipe from './routes/recipe'
+import tag from './routes/tag'
+import user from './routes/user'
 
 let app = express()
 
@@ -10,6 +18,14 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
-app.use('/menus', menus)
+app.use('/categories', category)
+app.use('/comments', comment)
+app.use('/ingredients', ingredient)
+app.use('/instructions', instruction)
+app.use('/menus', menu)
+app.use('/recipes', recipe)
+app.use('/tags', tag)
+app.use('/users', user)
 
 export default app
+
